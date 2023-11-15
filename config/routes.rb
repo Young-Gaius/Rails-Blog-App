6ac713316811373bd71f7779ad91081fcf19de31
Young-Gaius <youngehu8@gmail.com>
2023-11-15 14:03:52 +0100

    controler setup

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+end
