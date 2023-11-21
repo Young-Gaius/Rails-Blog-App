a06d51202e4cfb6b2d5d43201374c0b5657196b3
Young-Gaius <youngehu8@gmail.com>
2023-11-21 15:56:45 +0100

    add authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_102633) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_091152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_102633) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "role", default: "normal", null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -68,5 +69,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_102633) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users", column: "author_id"
+  
 end
